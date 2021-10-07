6d1da1fc486458db2cf98aa3552e1b878a6971ae
Rozen2007 <deedirozen@gmail.com>
2021-10-07 16:14:01 +0530

    MineCraft in python by Rozen

---
+++ Minecraft-Rozen/README.md
@@ -0,0 +1,6 @@
+# Miniecraft
+A kind of Minecraft game using Python =]
+### To run this game
+` pip install ursina `
+######
+` python UrsaCraft_video.py `
---
+++ Minecraft-Rozen/test.py
@@ -0,0 +1,46 @@
+from ursina import *
+
+
+class Test_cube(Entity):
+	def __init__(self):
+		super().__init__(
+			parent = scene,
+			model = 'cube',
+			texture = 'white_cube',
+			rotation = Vec3(45,45,45))
+
+
+def update():
+	#print('test')
+	if held_keys['a']:
+		cube.x -= 1 * time.dt
+
+class Test_button(Button):
+	def __init__(self,scale = 0.1):
+		super().__init__(
+			parent = scene,
+			model = 'cube',
+			texture = 'brick',
+			color = color.white,
+			highlight_color = color.red,
+			pressed_color = color.lime)
+
+	def input(self,key):
+		if self.hovered:
+			if key == 'left mouse down':
+				punch_sound.play()
+app = Ursina()
+
+
+cube = Entity(model='quad', color=color.orange, scale = (2,5), position = (5,1))
+
+
+
+test = Test_cube()
+
+
+btn = Test_button()
+punch_sound = Audio('assets/punch', loop=False, autoplay=False)
+
+app.run()
+
